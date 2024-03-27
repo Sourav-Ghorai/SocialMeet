e095ffc32eba0ecdc7973f336a772af7c9db3439
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-03-27 19:41:22 +0530

    Added initial files

---
+++ Backend/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import colors from "colors";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+const app = express();
+
+// Middleware configuration
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Mongoose setup
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server is running at port: ${PORT}`.bgCyan.black)
+    );
+  })
+  .catch((error) => console.log(`${error} did not connect`));

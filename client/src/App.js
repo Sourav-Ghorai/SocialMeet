416c410512d579661124bf2edac29e8dc280f423
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-03-29 14:55:45 +0530

    Added pages and setup redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "pages/HomePage";
+import LoginPage from "pages/LoginPage";
+import ProfilePage from "pages/ProfilePage";
+
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

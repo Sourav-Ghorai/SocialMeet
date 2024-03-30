215b4008c10f408e809bfa80a69dc981940831f9
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-03-30 22:00:59 +0530

    Added dark mode and light mode theme set up using mui

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "pages/HomePage";
 import LoginPage from "pages/LoginPage";
 import ProfilePage from "pages/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+
+  // Create and memoize the theme
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
+} from "@mui/icons-material";
+import { UseDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state/reduxState";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "./FlexBetween";
 
-function navbar() {
-  return (
-    <div>navbar</div>
-  )
+function Navbar() {
+  return <div>navbar</div>;
 }
 
-export default navbar
+export default Navbar;

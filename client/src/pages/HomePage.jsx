480f9011051ba991b1332e2996059f5a2a648abf
Sourav Ghorai <ghoraisourav451@gmail.com>
2024-08-16 02:07:13 +0530

    Modified

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Box, useMediaQuery } from "@mui/material";
-import Navbar from "components/Navbar";
+import Navbar from "../components/Navbar";
 import React from "react";
 import { useSelector } from "react-redux";
 import UserWidget from "./widgets/UserWidget";
